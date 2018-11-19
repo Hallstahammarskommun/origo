0bdc872d677e15f940eaca94e8e55dfb1d4935bf
Jonas <jonas.lundgren@hallstahammar.se>
2018-11-19 13:15:41 +0100

    Update from master: Show attribute as title

--- src/getfeatureinfo.js
+++ src/getfeatureinfo.js
@@ -200,7 +200,8 @@ function getFeaturesFromRemote(evt) {
           requestResult.push({
             title: layer.get('title'),
             feature,
-            content: getAttributes(feature, layer)
+            content: getAttributes(feature, layer),
+            layer: layer.get('name')
           });
         });
       } else {
@@ -245,6 +246,7 @@ function getFeaturesAtPixel(evt, clusterFeatureinfoLevel) {
           item.title = l.get('title');
           item.feature = f;
           item.content = getAttributes(f, l);
+          item.name = l.get('name');
           result.push(item);
         });
       } else if (collection.length === 1 && queryable) {
@@ -252,6 +254,7 @@ function getFeaturesAtPixel(evt, clusterFeatureinfoLevel) {
         item.title = l.get('title');
         item.feature = collection[0];
         item.content = getAttributes(collection[0], l);
+        item.name = l.get('name');
         item.layer = l;
         result.push(item);
       }
@@ -260,6 +263,7 @@ function getFeaturesAtPixel(evt, clusterFeatureinfoLevel) {
       item.title = l.get('title');
       item.feature = feature;
       item.content = getAttributes(feature, l);
+      item.name = l.get('name');
       item.layer = l;
       result.push(item);
     }
