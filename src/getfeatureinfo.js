f577f79379d6c51f974b739c00bc55618f0e7bbc
Jonas <jonas.lundgren@hallstahammar.se>
2019-01-07 10:24:49 +0100

    Koll för queryurl

--- src/getfeatureinfo.js
+++ src/getfeatureinfo.js
@@ -9,10 +9,16 @@ function getGetFeatureInfoUrl({
   resolution,
   projection
 }, layer) {
-  const url = layer.getSource().getGetFeatureInfoUrl(coordinate, resolution, projection, {
-    INFO_FORMAT: 'application/json',
-    FEATURE_COUNT: '20'
-  });
+  let url;
+  
+  if (layer.get('queryurl') !== undefined) {
+    url = `${layer.get('queryurl') + coordinate[1]},${coordinate[0]}`;
+  } else {
+    url = layer.getSource().getGetFeatureInfoUrl(coordinate, resolution, projection, {
+      INFO_FORMAT: 'application/json',
+      FEATURE_COUNT: '20'
+    });
+  }
 
   return $.ajax(url, {
     type: 'post'
