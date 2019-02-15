39d73bf56c4c46223a3f59d410363a4f77e528af
Jonas <jonas.lundgren@hallstahammar.se>
2019-02-15 14:59:09 +0100

    Splash

--- src/controls/splash.js
+++ src/controls/splash.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import { Component, Modal } from '../ui';
 
 const Splash = function Splash(options = {}) {
@@ -10,12 +11,40 @@ const Splash = function Splash(options = {}) {
   let {
     title,
     content,
-    target
+    target,
+    hideText,
+    confirmText
   } = options;
 
   const {
+    buttons,
     url
   } = options;
+
+  const addButtons = function addButtons() {
+    const visitButtons = `${'<br>'}<input id="o-splash-hide-button" type="button" value="${hideText}">`;
+    const bodyIndex = content.lastIndexOf('</body>');
+    if (bodyIndex !== -1) {
+      content = content.substring(0, bodyIndex) + visitButtons + content.substring(bodyIndex);
+    } else {
+      content += visitButtons;
+    }
+  };
+
+  const clearLocalStorage = function clearLocalStorage() {
+    localStorage.removeItem('splashVisibility');
+    localStorage.removeItem('splashContent');
+  };
+
+  const setLocalStorage = function setLocalStorage() {
+    const newContent = localStorage.getItem('splashContent') !== content;
+    if (localStorage.getItem('splashVisibility') !== 'false' || newContent) {
+      localStorage.setItem('splashContent', content);
+      if (newContent) {
+        localStorage.setItem('splashVisibility', 'true');
+      }
+    }
+  };
 
   return Component({
     name: 'splash',
@@ -23,13 +52,26 @@ const Splash = function Splash(options = {}) {
       viewer = evt.target;
       target = viewer.getId();
       if (!title) title = defaultTitle;
+      if (options.buttons) {
+        hideText = Object.prototype.hasOwnProperty.call(options.buttons, 'hideText') ? options.buttons.hideText : 'Visa inte igen';
+        confirmText = Object.prototype.hasOwnProperty.call(options.buttons, 'confirmText') ? options.buttons.confirmText : 'Är du säker på att du vill dölja den här informationen?';
+      }
 
       if (url) {
         const fullUrl = viewer.getBaseUrl() + url;
         const req = new Request(`${fullUrl}`);
         fetch(req).then(response => response.text().then((text) => {
           content = text;
-          this.render();
+          if (buttons) {
+            addButtons();
+            setLocalStorage();
+            if (localStorage.getItem('splashVisibility') === 'true') {
+              this.render();
+            }
+          } else {
+            clearLocalStorage();
+            this.render();
+          }
         }));
       } else {
         if (!content) content = defaultContent;
@@ -45,6 +87,13 @@ const Splash = function Splash(options = {}) {
       });
       this.addComponent(modal);
       this.dispatch('render');
+      $('#o-splash-hide-button').on('click', () => {
+        const proceed = window.confirm(confirmText);
+        if (proceed) {
+          modal.closeModal();
+          localStorage.setItem('splashVisibility', false);
+        }
+      });
     }
   });
 };
