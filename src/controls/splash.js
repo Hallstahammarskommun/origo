8dd4f709076e1669ea515bc25ca06b9ec2efbc0e
Markus <markus.meria@hallstahammar.se>
2019-02-28 10:42:19 +0100

    Origo ui kompatibel splash

--- src/controls/splash.js
+++ src/controls/splash.js
@@ -1,12 +1,13 @@
-import $ from 'jquery';
-import { Component, Modal } from '../ui';
+import { Component, Button, Modal } from '../ui';
 
 const Splash = function Splash(options = {}) {
   const defaultTitle = 'Om kartan';
   const defaultContent = '<p></p>';
   const cls = 'o-splash';
   let viewer;
+  let closeButton;
   let modal;
+  let component;
 
   let {
     title,
@@ -21,14 +22,10 @@ const Splash = function Splash(options = {}) {
     url
   } = options;
 
-  const addButtons = function addButtons() {
-    const visitButtons = `${'<br>'}<input id="o-splash-hide-button" type="button" value="${hideText}">`;
-    const bodyIndex = content.lastIndexOf('</body>');
-    if (bodyIndex !== -1) {
-      content = content.substring(0, bodyIndex) + visitButtons + content.substring(bodyIndex);
-    } else {
-      content += visitButtons;
-    }
+  const addButton = function addButton() {
+    const closeButtonHtml = closeButton.render();
+    content += closeButtonHtml;
+    return content;
   };
 
   const clearLocalStorage = function clearLocalStorage() {
@@ -46,54 +43,66 @@ const Splash = function Splash(options = {}) {
     }
   };
 
-  return Component({
-    name: 'splash',
-    onAdd(evt) {
-      viewer = evt.target;
-      target = viewer.getId();
-      if (!title) title = defaultTitle;
-      if (options.buttons) {
-        hideText = Object.prototype.hasOwnProperty.call(options.buttons, 'hideText') ? options.buttons.hideText : 'Visa inte igen';
-        confirmText = Object.prototype.hasOwnProperty.call(options.buttons, 'confirmText') ? options.buttons.confirmText : 'Är du säker på att du vill dölja den här informationen?';
-      }
+  const createModal = function createModal(modalContent) {
+    content = modalContent;
 
-      if (url) {
-        const fullUrl = viewer.getBaseUrl() + url;
-        const req = new Request(`${fullUrl}`);
-        fetch(req).then(response => response.text().then((text) => {
-          content = text;
-          if (buttons) {
-            addButtons();
-            setLocalStorage();
-            if (localStorage.getItem('splashVisibility') === 'true') {
-              this.render();
-            }
-          } else {
-            clearLocalStorage();
-            this.render();
-          }
-        }));
-      } else {
-        if (!content) content = defaultContent;
-        this.render();
-      }
-    },
-    render() {
+    if (buttons) {
+      setLocalStorage();
+      component.addComponent(closeButton);
+      content = addButton();
+    } else {
+      clearLocalStorage();
+    }
+
+    if (localStorage.getItem('splashVisibility') !== 'false') {
       modal = Modal({
         title,
         content,
         cls,
         target
       });
-      this.addComponent(modal);
-      this.dispatch('render');
-      $('#o-splash-hide-button').on('click', () => {
-        const proceed = window.confirm(confirmText);
-        if (proceed) {
-          modal.closeModal();
-          localStorage.setItem('splashVisibility', false);
-        }
-      });
+      component.dispatch('render');
+    }
+  };
+
+  return Component({
+    name: 'splash',
+    onInit() {
+      if (!title) title = defaultTitle;
+      if (!content) content = defaultContent;
+      if (options.buttons) {
+        hideText = Object.prototype.hasOwnProperty.call(options.buttons, 'hideText') ? options.buttons.hideText : 'Visa inte igen';
+        confirmText = Object.prototype.hasOwnProperty.call(options.buttons, 'confirmText') ? options.buttons.confirmText : 'Är du säker på att du vill dölja den här informationen?';
+      }
+      if (buttons) {
+        closeButton = Button({
+          cls: 'rounded margin-top-small padding-y grey-lightest',
+          style: 'display: block;',
+          text: hideText,
+          click() {
+            const proceed = window.confirm(confirmText);
+            if (proceed) {
+              modal.closeModal();
+              localStorage.setItem('splashVisibility', false);
+            }
+          }
+        });
+      }
+    },
+    onAdd(evt) {
+      component = this;
+      viewer = evt.target;
+      target = viewer.getId();
+      if (url) {
+        const fullUrl = viewer.getBaseUrl() + url;
+        const req = new Request(`${fullUrl}`);
+        fetch(req).then(response => response.text().then((text) => {
+          content = text;
+          createModal(content);
+        }));
+      } else {
+        createModal(content);
+      }
     }
   });
 };
