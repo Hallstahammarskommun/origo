a1cbf95f97b3da587de7756533f3bb6d36f6c8d8
Jonas <jonas.lundgren@hallstahammar.se>
2020-07-22 11:38:51 +0200

    Update replacer.js

--- src/utils/replacer.js
+++ src/utils/replacer.js
@@ -35,7 +35,8 @@ const replacer = function replacer() {
           object = obj;
         }
       }
-      return searchAndReplace(name.replace(matches[0], val), object, map);
+      // Z
+      return searchAndReplace(name.replace(matches[0], val), obj, map);
     }
     return name;
   }
