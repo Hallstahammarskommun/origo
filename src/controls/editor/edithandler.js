1aa0f77a7f5b5e0bd20c7227443c6e8cf252fc49
Jonas <jonas.lundgren@hallstahammar.se>
2021-01-07 10:27:20 +0100

    username som attribut
    Skickar med användarnamn som attribut i editorn

--- src/controls/editor/edithandler.js
+++ src/controls/editor/edithandler.js
@@ -82,6 +82,8 @@ function getFeaturesByIds(type, layer, ids) {
       if (source.getFeatureById(id)) {
         feature = source.getFeatureById(id);
         feature.unset('bbox');
+        const username = localStorage.getItem('username');
+        feature.values_.user = username;
         features.push(feature);
       }
     });
