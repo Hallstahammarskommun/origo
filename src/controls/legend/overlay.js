e72fe8e933cbd8f1e036b08d819e5922b854599e
Jonas <jonas.lundgren@hallstahammar.se>
2022-11-11 12:04:20 +0100

    II

--- src/controls/legend/overlay.js
+++ src/controls/legend/overlay.js
@@ -69,6 +69,20 @@ const OverlayLayer = function OverlayLayer(options) {
       layers.forEach(l => l.setVisible(false));
     }
     layer.setVisible(!visible);
+    if (!visible) {
+      const fmeTitle = layer.get('title')
+      let fmeLayerUrl = 'https://karta.hallstahammar.se/fmejobsubmitter/Script/Layerlog.fmw?'
+      fmeLayerUrl += `username=${localStorage.getItem('username')}`;
+      fmeLayerUrl += `&layer=${fmeTitle}`;
+      fmeLayerUrl += '&DestDataset_POSTGRES=PostGIS%20geodata&opt_showresult=false&opt_servicemode=sync&token=0faad637053aa36f6a1cb4b5adf3d5401bfd0bd7';
+      function httpGet(fmeLayerUrl) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", fmeLayerUrl, true); // false for synchronous request
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+      }
+      httpGet(fmeLayerUrl)
+    }
     return !visible;
   };
 
