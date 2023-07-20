68099e185d60e878443ab36351a988f24e1e14c2
Jonas <jonas.lundgren@hallstahammar.se>
2023-07-20 09:49:12 +0200

    Update search.js

--- src/controls/search.js
+++ src/controls/search.js
@@ -351,19 +351,6 @@ const Search = function Search(options = {}) {
       if (includeSearchableLayers) {
         queryUrl += `&l=${viewer.getSearchableLayers(searchableDefault)}`;
       }
-      let interactionLogURL = 'https://karta.hallstahammar.se/fmejobsubmitter/Script/tracker%20interaction.fmw?';
-      interactionLogURL += `username=${localStorage.getItem('username')}`;
-      interactionLogURL += '&obj=Search';
-      interactionLogURL += `&typ=${obj.value}`;
-      interactionLogURL += '&DestDataset_POSTGRES=PostgreSQL%20geodata&opt_showresult=false&opt_servicemode=sync&token=46b87662b6a67111c1386a1d37e03d01e394a3b1';
-
-      fetch(interactionLogURL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/xml; charset=UTF-8'
-        }
-      });
-
       fetch(queryUrl)
         .then(response => response.json())
         .then((data) => {
