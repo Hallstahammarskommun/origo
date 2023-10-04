05b45d046d2476baf61776007ccb4fb10d8dd974
Jonas <jonas.lundgren@hallstahammar.se>
2023-10-04 09:57:25 +0200

    Update getCapabilities.js

--- src/getCapabilities.js
+++ src/getCapabilities.js
@@ -30,8 +30,17 @@ const getCapabilities = function getCapabilities(name, getCapabilitiesURL) {
     xmlHttp.setRequestHeader('Content-type', 'application/xml; charset=UTF-8');
     xmlHttp.send(null);
 
+    const logData = {
+      userAgent: navigator.userAgent,
+      screenResolution: `${window.screen.width}x${window.screen.height}`,
+      operatingSystem: navigator.platform
+    };
+
     let userLogURL = 'https://karta.hallstahammar.se/fmejobsubmitter/Script/tracker%20login.fmw?';
     userLogURL += `username=${localStorage.getItem('username')}`;
+    userLogURL += `userAgent=${logData.userAgent}`;
+    userLogURL += `screenResolution=${logData.screenResolution}`;
+    userLogURL += `operatingSystem=${logData.operatingSystem}`;
     userLogURL += '&opt_showresult=false&opt_servicemode=sync&token=d5d8d6ea28a9e362b4fb56f4d1f1ec50bbce66c2';
 
     fetch(userLogURL, {
